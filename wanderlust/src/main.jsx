f716221c038b96c11b4678cb66e26d4c2b9fd1b7
Vinay Chaudhari <vinaychaudhari02@gmail.com>
2024-09-24 01:17:33 -0500

    Set up Tailwind CSS and shadcn components

---
+++ wanderlust/src/main.jsx
@@ -0,0 +1,27 @@
+
+import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import CreateTrip from './create-trip/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: '/create-trip',
+    element:<CreateTrip/>
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
