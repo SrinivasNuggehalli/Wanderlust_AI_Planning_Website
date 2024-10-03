42d399b7cc568d93a39cb1a70ed3af17d23ca2c0
Sarvesh-7777 <sonawanesarvesh19@gmail.com>
2024-10-03 00:42:24 -0500

    Updated Hero and Header section layout and responsiveness

--- wanderlust/src/components/custom/Header.jsx
+++ wanderlust/src/components/custom/Header.jsx
@@ -3,12 +3,17 @@ import { Button } from '../ui/button'
 
 function Header() {
   return (
-    <div className='p-3 shadow-sm flex justify-between items-center px-5'>
-      <img src = '/logo.svg'/>
-      <div>
-        <Button>Sign in</Button>
+    <header className="w-full bg-white shadow-md">
+      <div className="max-w-screen-xl mx-auto flex justify-between items-center p-4">
+        {/* Logo */}
+        <img src="/logo.svg" alt="Logo" className="h-10" />
+
+        {/* Sign in button */}
+        <div>
+          <Button>Sign in</Button>
+        </div>
       </div>
-    </div>
+    </header>
   )
 }
 
