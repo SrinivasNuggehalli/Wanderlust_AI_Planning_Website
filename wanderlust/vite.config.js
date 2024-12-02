aa6dade860a6733aee1bc5f379875aee569fd103
SrinivasNuggehalli <157188461+SrinivasNuggehalli@users.noreply.github.com>
2024-12-02 10:52:53 -0600

    Update vite.config.js

--- wanderlust/vite.config.js
+++ wanderlust/vite.config.js
@@ -10,8 +10,11 @@ export default defineConfig({
     },
   },
   server: {
-    host: '0.0.0.0',
+    host: true,
     port: 5173,
-    strictPort: true
+    strictPort: true,
+    watch: {
+      usePolling: true
+    }
   }
 })
