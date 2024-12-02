b0e6bb62c703f83462dd0e1d3d5ece77763273b8
SrinivasNuggehalli <157188461+SrinivasNuggehalli@users.noreply.github.com>
2024-12-02 11:16:30 -0600

    Update vite.config.js

--- wanderlust/vite.config.js
+++ wanderlust/vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -10,12 +10,12 @@ export default defineConfig({
     },
   },
   server: {
-    host: '0.0.0.0',
+    host: true, // Allows the server to accept requests from any host
     port: 5173,
     strictPort: true,
     hmr: {
+      protocol: "ws", // Use WebSocket for HMR
       clientPort: 5173,
-      host: '0.0.0.0'
-    }
-  }
-})
+    },
+  },
+});
